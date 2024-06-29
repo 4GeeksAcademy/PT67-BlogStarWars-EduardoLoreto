678f3a967db936871a81e49c5392aee49f5d601a
Eduardo Loreto <138321528+EduardoLoreto@users.noreply.github.com>
2024-06-29 20:39:46 +0000

    Creacion de Personajes, Vehiculos y Planetas

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,65 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+	  store: {
+		people: [],
+		vehicles: [],
+		planets: [],
+	  },
+	  actions: {
+		loadPeople: () => {
+		  const peoplePromises = Array.from({ length: 4 }, (_, i) =>
+			fetch(`https://www.swapi.tech/api/people/${i + 1}`)
+			  .then((response) => response.json())
+			  .then((data) => data.result)
+			  .catch((error) => console.error(error))
+		  );
+  
+		  Promise.all(peoplePromises)
+			.then((people) => setStore({ ...getStore(), people }))
+			.catch((error) => console.error(error));
 		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
+		loadPlanets: () => {
+		  const planetsPromises = Array.from({ length: 4 }, (_, i) =>
+			fetch(`https://www.swapi.tech/api/planets/${i + 1}`)
+			  .then((response) => response.json())
+			  .then((data) => data.result)
+			  .catch((error) => console.error(error))
+		  );
+  
+		  Promise.all(planetsPromises)
+			.then((planets) => setStore({ ...getStore(), planets }))
+			.catch((error) => console.error(error));
+		},
+  
+		loadVehicles: () => {
+		  const vehicleIds = [4, 6, 7, 8];
+		  const vehiclesPromises = vehicleIds.map((id) =>
+			fetch(`https://www.swapi.tech/api/vehicles/${id}`)
+			  .then((response) => response.json())
+			  .then((data) => data.result)
+			  .catch((error) => console.error(error))
+		  );
+  
+		  Promise.all(vehiclesPromises)
+			.then((vehicles) => setStore({ ...getStore(), vehicles }))
+			.catch((error) => console.error(error));
+		},
+  
+		addFavorite: (item) => {
+		  const store = getStore();
+		  const favorites = store.favorites || [];
+		  setStore({ ...store, favorites: [...favorites, item] });
+		},
+		removeFavorite: (item) => {
+		  const store = getStore();
+		  const favorites = store.favorites || [];
+		  setStore({
+			...store,
+			favorites: favorites.filter((fav) => fav !== item),
+		  });
+		},
+	  },
 	};
-};
-
-export default getState;
+  };
+  
+  export default getState;
